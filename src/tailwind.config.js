f51be44bc50fac3ee192db00da9423203daa051b
Asher Myers <43706372+ashermyers@users.noreply.github.com>
2023-10-03 18:17:24 -0400

    Preperation

--- src/tailwind.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  content: ["./pages/**/*.{js,jsx}", "./components/**/*.{js,jsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
-};
